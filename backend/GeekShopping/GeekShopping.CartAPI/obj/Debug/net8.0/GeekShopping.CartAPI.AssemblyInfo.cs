ae64faf867c4a22aba37f8ee6067ccbe19df13bf
Leonardo Ferreira <leonardoferreira032@gmail.com>
2024-05-10 18:32:20 -0300

    Criando projeto e adicionando dependências

--- backend/GeekShopping/GeekShopping.CartAPI/obj/Debug/net8.0/GeekShopping.CartAPI.AssemblyInfo.cs
+++ backend/GeekShopping/GeekShopping.CartAPI/obj/Debug/net8.0/GeekShopping.CartAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GeekShopping.CartAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7d695ca0d628135f45ce8e4152847e39f5df8b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d07e2abbe90152495fb120f55cca39f281188f5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("GeekShopping.CartAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GeekShopping.CartAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- backend/GeekShopping/GeekShopping.Identity/obj/Debug/net8.0/GeekShopping.Identity.AssemblyInfo.cs
+++ backend/GeekShopping/GeekShopping.Identity/obj/Debug/net8.0/GeekShopping.Identity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GeekShopping.Identity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3da6d0b60e340dabfe64cb6dd81508e9065242d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d07e2abbe90152495fb120f55cca39f281188f5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("GeekShopping.Identity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GeekShopping.Identity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
