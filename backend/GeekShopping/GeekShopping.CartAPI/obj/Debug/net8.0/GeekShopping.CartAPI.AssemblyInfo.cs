7e7e5e4b97cd287690cbbb5f6a787652a58ea24b
Leonardo Ferreira <leonardoferreira032@gmail.com>
2024-05-14 20:00:13 -0300

    Preparando ambiente para consumir a api de coupon

--- backend/GeekShopping/GeekShopping.CartAPI/obj/Debug/net8.0/GeekShopping.CartAPI.AssemblyInfo.cs
+++ backend/GeekShopping/GeekShopping.CartAPI/obj/Debug/net8.0/GeekShopping.CartAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GeekShopping.CartAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14e199230c8d63d0ad2c7017bfc186b30786bddf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df325a88f9e558b201244842c57960cc64e5e6ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("GeekShopping.CartAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GeekShopping.CartAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
