917769d60d3398dac67e8e2fabecb9b742936cf1
Leonardo Ferreira <leonardoferreira032@gmail.com>
2024-05-19 16:23:57 -0300

    Preparando projeto para executar com docker

--- backend/GeekShopping/GeekShopping.CartAPI/obj/Debug/net8.0/GeekShopping.CartAPI.AssemblyInfo.cs
+++ backend/GeekShopping/GeekShopping.CartAPI/obj/Debug/net8.0/GeekShopping.CartAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GeekShopping.CartAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec98d391677f75b82e8e1442d0d726200a03c6c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c420d49b2cc6d712a94e6c2cdc9e6a1b0403656")]
 [assembly: System.Reflection.AssemblyProductAttribute("GeekShopping.CartAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GeekShopping.CartAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
