bbb591f837615d3ebdd01d58ab2e097979170eea
Leonardo Ferreira <leonardoferreira032@gmail.com>
2024-05-22 17:45:40 -0300

    Validação de cpf

--- backend/GeekShopping/GeekShopping.Identity/Model/User.cs
+++ backend/GeekShopping/GeekShopping.Identity/Model/User.cs
@@ -31,5 +31,97 @@ namespace GeekShopping.Identity.Model
         [Column("role_id")]
         public int RoleId { get; set; }
         public Role? Role { get; set; }
+
+        public static bool ValidateCpf(string cpf)
+        {
+            try
+            {
+                if (cpf.Length != 11 || AvoidSequence(cpf))
+                {
+                    return false;
+                }
+
+                string newCpf = cpf.Substring(0, 9);
+                int total = 0;
+                int reverse = 10;
+                int sequence = 27;
+
+                for (int i = 0; i <= sequence; i++)
+                {
+                    if (i > 8 && sequence == 18)
+                    {
+                        i -= 9;
+                    }
+
+                    total += Convert.ToInt32(newCpf[i].ToString()) * reverse;
+
+                    reverse -= 1;
+                    if (reverse < 2)
+                    {
+                        reverse = 11;
+                        double digito = 11 - (total % 11);
+
+                        if (digito > 9)
+                        {
+                            digito = 0;
+                        }
+
+                        newCpf += Convert.ToString(digito);
+                        total = 0;
+                    }
+                    sequence -= 1;
+                }
+
+                if (!(newCpf == cpf))
+                {
+                    return false;
+                }
+
+                return true;
+            }
+            catch (FormatException e)
+            {
+                //capturando error de letras digitadas no campo cpf
+                return false;
+            }
+        }
+
+        private static bool AvoidSequence(string cpf)
+        {
+            switch (cpf)
+            {
+                case "11111111111":
+                    return true;
+                    break;
+                case "22222222222":
+                    return true;
+                    break;
+                case "33333333333":
+                    return true;
+                    break;
+                case "44444444444":
+                    return true;
+                    break;
+                case "55555555555":
+                    return true;
+                    break;
+                case "66666666666":
+                    return true;
+                    break;
+                case "77777777777":
+                    return true;
+                    break;
+                case "88888888888":
+                    return true;
+                    break;
+                case "99999999999":
+                    return true;
+                    break;
+                case "00000000000":
+                    return true;
+                    break;
+            }
+            return false;
+        }
     }
 }
--- backend/GeekShopping/GeekShopping.Identity/obj/Debug/net8.0/GeekShopping.Identity.AssemblyInfo.cs
+++ backend/GeekShopping/GeekShopping.Identity/obj/Debug/net8.0/GeekShopping.Identity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GeekShopping.Identity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ea850bdf84f91bdcf12256694fa3987bf07f439")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56ce4b4784012501be7b483f0a87705bf65de5d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("GeekShopping.Identity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GeekShopping.Identity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
