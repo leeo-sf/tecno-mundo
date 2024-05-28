ebe080043400547a31786d52b58421353bbc520c
Leonardo Ferreira <leonardoferreira032@gmail.com>
2024-05-28 17:44:44 -0300

    Criando footer

--- TecnoMundo/TecnoMundo.Web/src/app/app.component.ts
+++ TecnoMundo/TecnoMundo.Web/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './page/header/header.component';
+import { FooterComponent } from './page/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
