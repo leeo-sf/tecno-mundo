1c181713da07c676e16abb12d832ddc560e86d4a
Leonardo Ferreira <leonardoferreira032@gmail.com>
2024-05-29 20:25:03 -0300

    Padrinizando icons angular material

--- TecnoMundo/TecnoMundo.Web/src/app/page/header/header.component.ts
+++ TecnoMundo/TecnoMundo.Web/src/app/page/header/header.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
+    MatIconModule,
     RouterLink
   ],
   templateUrl: './header.component.html',
