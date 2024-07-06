0be090ffa2630331e13fda678865248dbb62d2ae
Leonardo Ferreira <leonardoferreira032@gmail.com>
2024-07-05 23:10:50 -0300

    build solution

--- TecnoMundo/TecnoMundo.IdentityAPI/obj/Debug/net8.0/TecnoMundo.IdentityAPI.AssemblyInfo.cs
+++ TecnoMundo/TecnoMundo.IdentityAPI/obj/Debug/net8.0/TecnoMundo.IdentityAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TecnoMundo.IdentityAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f960883ecd5c765684bb2523da6a6fa72aa0b03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3aaa7356ce5f4422236a42df2da785a6031f8c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("TecnoMundo.IdentityAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TecnoMundo.IdentityAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- TecnoMundo/TecnoMundo.ProductAPI/obj/Debug/net8.0/TecnoMundo.ProductAPI.AssemblyInfo.cs
+++ TecnoMundo/TecnoMundo.ProductAPI/obj/Debug/net8.0/TecnoMundo.ProductAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TecnoMundo.ProductAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2fd541442e3f4cf9e5cfbcefb39644250d13814")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3aaa7356ce5f4422236a42df2da785a6031f8c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("TecnoMundo.ProductAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TecnoMundo.ProductAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
