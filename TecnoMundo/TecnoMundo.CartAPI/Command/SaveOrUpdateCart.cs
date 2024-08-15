df40ce21ffffdd0e2b13febe5efbeaa41f63c5f6
Leonardo Ferreira <leonardoferreira032@gmail.com>
2024-08-14 22:34:36 -0300

    corrigindo update count cart

--- TecnoMundo/TecnoMundo.CartAPI/Command/SaveOrUpdateCart.cs
+++ TecnoMundo/TecnoMundo.CartAPI/Command/SaveOrUpdateCart.cs
@@ -61,7 +61,6 @@ namespace GeekShopping.CartAPI.Command
                 else
                 {
                     cart.CartDetails.FirstOrDefault().Product = null;
-                    cart.CartDetails.FirstOrDefault().Count = cartDetail.Count;
                     cart.CartDetails.FirstOrDefault().Id = cartDetail.Id;
                     cart.CartDetails.FirstOrDefault().CartHeaderId = cartDetail.CartHeaderId;
                     await _repository.UpdateCartDetails(cart.CartDetails.FirstOrDefault());
