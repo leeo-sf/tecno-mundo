5e863215ca17911e6054d6fd16180e3bdcd566e5
Leonardo Ferreira <leonardoferreira032@gmail.com>
2024-09-06 10:30:57 -0300

    Swagger somente em desenvolvimento

--- TecnoMundo/TecnoMundo.CartAPI/Program.cs
+++ TecnoMundo/TecnoMundo.CartAPI/Program.cs
@@ -134,8 +134,12 @@ builder.Services.AddSwaggerGen(c =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseSwagger();
-app.UseSwaggerUI();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
