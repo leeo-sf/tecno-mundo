f9354547dd2eba643f3cf7a632fa5b9b8a11dd4f
Leonardo Ferreira <leonardoferreira032@gmail.com>
2024-10-28 21:25:18 -0300

    adicionando services de cache in application

---
+++ src/Core/TecnoMundo.Application/Caching/CachingService.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace TecnoMundo.ProductAPI.Caching
+{
+    public class CachingService : ICachingService
+    {
+        private readonly IDistributedCache _distributedCache;
+
+        public CachingService(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        public async Task<List<T>> GetListCache<T>(string keyCache)
+        {
+            var itemList = await _distributedCache.GetAsync(keyCache);
+
+            if (itemList is not null)
+            {
+                var serialize = Encoding.UTF8.GetString(itemList);
+                return JsonConvert.DeserializeObject<List<T>>(serialize) ?? [];
+            }
+
+            return [];
+        }
+
+        public async Task<T?> GetItemInCache<T>(string keyCache)
+        {
+            var item = await _distributedCache.GetAsync(keyCache);
+
+            if (item is not null)
+            {
+                var serialize = Encoding.UTF8.GetString(item);
+                return JsonConvert.DeserializeObject<T>(serialize);
+            }
+
+            return default(T);
+        }
+
+        public async Task<List<T>> AddListInCache<T>(List<T> item, string keyCache, DistributedCacheEntryOptions options)
+        {
+            var serializeItem = JsonConvert.SerializeObject(item);
+            var redisProducts = Encoding.UTF8.GetBytes(serializeItem);
+
+            await _distributedCache.SetAsync(keyCache, redisProducts, options);
+
+            return await GetListCache<T>(keyCache);
+        }
+
+        public async Task AddItemToExistingListInCache<T>(T item, string keyCache, DistributedCacheEntryOptions options)
+        {
+            var itemListInCache = await GetListCache<T>(keyCache);
+
+            if (itemListInCache.Count == 0) return;
+
+            itemListInCache.Add(item);
+            await AddListInCache(itemListInCache, keyCache, options);
+        }
+
+        public async Task<T?> AddItemInCache<T>(T item, string keyCache, DistributedCacheEntryOptions options)
+        {
+            var serializeItem = JsonConvert.SerializeObject(item);
+            var redisProducts = Encoding.UTF8.GetBytes(serializeItem);
+
+            await _distributedCache.SetAsync(keyCache, redisProducts, options);
+
+            return await GetItemInCache<T>(keyCache);
+        }
+
+        public async Task RemoveExistingListItemFromCache<T>(T item, string keyCache, DistributedCacheEntryOptions options)
+        {
+            var listCache = await GetListCache<T>(keyCache);
+
+            if (listCache.Count == 0) return;
+
+            listCache.Remove(item);
+            var updateList = listCache.ToList();
+            await AddListInCache(updateList, keyCache, options);
+        }
+    }
+}
---
+++ src/Core/TecnoMundo.Application/Caching/ICachingService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+
+namespace TecnoMundo.ProductAPI.Caching
+{
+    public interface ICachingService
+    {
+        Task<List<T>> AddListInCache<T>(List<T> item, string keyCache, DistributedCacheEntryOptions options);
+        Task<List<T>> GetListCache<T>(string keyCache);
+        Task<T?> AddItemInCache<T>(T item, string keyCache, DistributedCacheEntryOptions options);
+        Task AddItemToExistingListInCache<T>(T item, string keyCache, DistributedCacheEntryOptions options);
+        Task RemoveExistingListItemFromCache<T>(T item, string keyCache, DistributedCacheEntryOptions options);
+        Task<T?> GetItemInCache<T>(string keyCache);
+    }
+}
